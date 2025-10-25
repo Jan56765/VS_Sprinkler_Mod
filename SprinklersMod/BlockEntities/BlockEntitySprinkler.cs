2fbdb647d79e81d93309e702bca4dde2dbbfda31
Jan <janphilippburian@gmail.com>
2025-10-25 12:03:39 +0200

    refac and add translations for vertical range

--- SprinklersMod/BlockEntities/BlockEntitySprinkler.cs
+++ SprinklersMod/BlockEntities/BlockEntitySprinkler.cs
@@ -9,13 +9,10 @@ using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 using Vintagestory.GameContent;
 
-namespace SprinklersMod.BlockEntities
-{
-    public class BlockEntitySprinkler : BlockEntity
-    {
+namespace SprinklersMod.BlockEntities {
+    public class BlockEntitySprinkler : BlockEntity {
 
-        BlockEntityAnimationUtil animUtil
-        {
+        BlockEntityAnimationUtil animUtil {
             get { return GetBehavior<BEBehaviorAnimatable>().animUtil; }
         }
 
@@ -32,15 +29,13 @@ namespace SprinklersMod.BlockEntities
         private const float LATCH_UPPER_LIMIT = 0.95f;
 
 
-        public override void Initialize(ICoreAPI api)
-        {
+        public override void Initialize(ICoreAPI api) {
             base.Initialize(api);
 
             //Safety net if server and client get out of sync with this block Entity
             //In the past this causes invisible sprinklers registered as an air block
             //to cause the animUtil to throw a NullPointer
-            if (Block.IsMissing || !Block.Code.Path.StartsWith("t_"))
-            {
+            if (Block.IsMissing || !Block.Code.Path.StartsWith("t_")) {
                 Api.World.BlockAccessor.RemoveBlockEntity(Pos);
                 return; //Also DON'T register!!!
             }
@@ -52,12 +47,10 @@ namespace SprinklersMod.BlockEntities
             RegisterGameTickListener(OnGameTick, getRandomInterval());
         }
 
-        private void OnGameTick(float dt)
-        {
+        private void OnGameTick(float dt) {
             bool dirty = false;
             //Only run Script on actually filled sprinklers
-            if (waterAmount >= Math.Max(1, SprinklersModSystem.config.waterConsumption))
-            {
+            if (waterAmount >= Math.Max(1, SprinklersModSystem.config.waterConsumption)) {
                 bool wateredAtLeastOneBlock = false;
 
                 //Define Sprinkler Range in Blocks. In its current form it will always be square
@@ -68,12 +61,9 @@ namespace SprinklersMod.BlockEntities
                 List<BlockPos> blockList = new List<BlockPos>();
                 int scanningRangeVert = isDownFacing ? downwardsRange : 1;
                 BlockPos bottomBlock = Pos.DownCopy(scanningRangeVert);
-                for (int i = -range; i <= range; i++)
-                {
-                    for (int k = -range; k <= range; k++)
-                    {
-                        if (!isDownFacing && i == 0 && k == 0)
-                        {
+                for (int i = -range; i <= range; i++) {
+                    for (int k = -range; k <= range; k++) {
+                        if (!isDownFacing && i == 0 && k == 0) {
                             //Don't care for the block directly beneath
                             continue;
                         }
@@ -83,19 +73,15 @@ namespace SprinklersMod.BlockEntities
                 float blockMoistures = 0.0f;
                 float blockCount = 0.0f;
                 //Water (or attempt to) each block that has been selected
-                foreach (BlockPos bPos in blockList)
-                {
+                foreach (BlockPos bPos in blockList) {
                     Block b = Api.World.BlockAccessor.GetBlock(bPos);
-                    if (b.GetType() == typeof(BlockFarmland))
-                    {
+                    if (b.GetType() == typeof(BlockFarmland)) {
                         BlockEntityFarmland be = Api.World.BlockAccessor.GetBlockEntity<BlockEntityFarmland>(bPos);
                         blockCount++;
-                        if (be.MoistureLevel < LATCH_UPPER_LIMIT && !overfillLatch)
-                        {
+                        if (be.MoistureLevel < LATCH_UPPER_LIMIT && !overfillLatch) {
                             be.WaterFarmland(0.1f, false);
                             wateredAtLeastOneBlock = true;
-                            if (Api.World.Side == EnumAppSide.Client)
-                            {
+                            if (Api.World.Side == EnumAppSide.Client) {
                                 spawnParticles(bPos);
                             }
                             be.MarkDirty(true);
@@ -104,59 +90,50 @@ namespace SprinklersMod.BlockEntities
                     }
                 }
                 float newWaterPercentage = blockMoistures / blockCount;
-                if (newWaterPercentage != avgWaterPercentage)
-                {
+                if (newWaterPercentage != avgWaterPercentage) {
                     avgWaterPercentage = newWaterPercentage;
                     
                     dirty = true;
                 }
-                if (wateredAtLeastOneBlock)
-                {
-                    if (Api.World.Side == EnumAppSide.Client)
-                    {
+                if (wateredAtLeastOneBlock) {
+                    if (Api.World.Side == EnumAppSide.Client) {
                         playSound();
                         runAnimation("sprinkler-turn");
                     }
                     waterAmount -= Math.Max(1, SprinklersModSystem.config.waterConsumption);
-                    if (avgWaterPercentage >= LATCH_UPPER_LIMIT)
-                    {
+                    if (avgWaterPercentage >= LATCH_UPPER_LIMIT) {
                         overfillLatch = true;
                     }
                     dirty = true;
                 }
-                else if (overfillLatch && avgWaterPercentage < LATCH_LOWER_LIMIT)
-                {
+                else if (overfillLatch && avgWaterPercentage < LATCH_LOWER_LIMIT) {
                     overfillLatch = false;
                     dirty = true;
                 }
-                if (dirty)
-                {
+                if (dirty) {
                     MarkDirty();
                 }
             }
         }
 
         //Provide data to the Block Info Interface
-        public override void GetBlockInfo(IPlayer forPlayer, StringBuilder dsc)
-        {
+        public override void GetBlockInfo(IPlayer forPlayer, StringBuilder dsc) {
             dsc.Append(
                 string.Format("{0}: {1} / {2} {3}\n",
                 T("waterfilled"), //Translation for Water Filled
-                ((float)waterAmount / 10).ToString("0.#"), //Current Water Amount
+                ((float) waterAmount / 10).ToString("0.#"), //Current Water Amount
                 (volume / 10).ToString(), //Max Water Amount
                 T("liters")) //Translation for Liters
             );
             dsc.Append(T("averagemoisture") + ": " + (avgWaterPercentage * 100).ToString("0.#") + "%\n");
-            if (isDownFacing && downwardsRange > 0)
-            {
-                dsc.Append("Range: " + downwardsRange.ToString());
+            if (isDownFacing && downwardsRange > 0) {
+                dsc.Append(T("range") + ": " + downwardsRange.ToString());
             }
             dsc.ToString();
         }
 
         //Caching to persist data between world loads
-        public override void ToTreeAttributes(ITreeAttribute tree)
-        {
+        public override void ToTreeAttributes(ITreeAttribute tree) {
             base.ToTreeAttributes(tree);
             tree.SetInt("waterAmount", waterAmount);
             tree.SetFloat("avgWaterPercentage", avgWaterPercentage);
@@ -164,8 +141,7 @@ namespace SprinklersMod.BlockEntities
             tree.SetBool("overfillLatch", overfillLatch);
         }
 
-        public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldForResolving)
-        {
+        public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldForResolving) {
             base.FromTreeAttributes(tree, worldForResolving);
             waterAmount = tree.GetInt("waterAmount", 0);
             avgWaterPercentage = tree.GetFloat("avgWaterPercentage", 0.0f);
@@ -173,62 +149,48 @@ namespace SprinklersMod.BlockEntities
             overfillLatch = tree.GetBool("overfillLatch", avgWaterPercentage >= 0.95);
         }
 
-        public override bool OnTesselation(ITerrainMeshPool mesher, ITesselatorAPI tessThreadTesselator)
-        {
+        public override bool OnTesselation(ITerrainMeshPool mesher, ITesselatorAPI tessThreadTesselator) {
         
             //I don't understand exactly why it's here, I just assume it's because this guarantees that the animator
             //will only be initialized on the client side (Example taken from the Riftward)
             //Also only works here and NOT in the Init function
-            if (animUtil?.animator == null)
-            {
+            if (animUtil?.animator == null) {
                 animUtil?.InitializeAnimator("sprinkler");
             }
             return base.OnTesselation(mesher, tessThreadTesselator);
         }
 
-        public void fillWater(int byAmount)
-        {
-            if (waterAmount + byAmount > volume)
-            {
+        public void fillWater(int byAmount) {
+            if (waterAmount + byAmount > volume) {
                 waterAmount = volume;
-            }
-            else
-            {
+            } else {
                 waterAmount += byAmount;
             }
             MarkDirty();
         }
 
-        public int getMissingWater()
-        {
+        public int getMissingWater() {
             return volume - waterAmount;
         }
 
-        public void adjustVertRange()
-        {
+        public void adjustVertRange() {
             //Adjust range downwards only for down facing sprinklers
-            if (downwardsRange >= 6)
-            {
+            if (downwardsRange >= 6) {
                 downwardsRange = 3;
-            }
-            else
-            {
+            } else {
                 downwardsRange++;
             }
             MarkDirty();
         }
         
         //Translations
-        private static string T(string code)
-        {
+        private static string T(string code) {
             return Lang.Get("sprinklersmod:" + code);
         }
 
-        private void spawnParticles(BlockPos p)
-        {
+        private void spawnParticles(BlockPos p) {
             SimpleParticleProperties WaterParticles;
-            if (!isDownFacing)
-            {
+            if (!isDownFacing) {
                 //This particle Spawner will calculate the distance from the sprinkler to the acre
                 //and then yeet a sploosh of water in that direction
                 WaterParticles = new SimpleParticleProperties(
@@ -245,8 +207,7 @@ namespace SprinklersMod.BlockEntities
                     /* maxSize */ 1.4f,
                     /* model */ EnumParticleModel.Cube
                 );
-            } else
-            {
+            } else {
                 //For the life of me, I couldn't figure the right trajectory out for downwards facing
                 //As I'm not a huge math nerd this will have to do
                 //Spawns litte water splooshs directly emitting from the acre
@@ -272,33 +233,27 @@ namespace SprinklersMod.BlockEntities
             Api.World.SpawnParticles(WaterParticles, null);
         }
 
-        private void runAnimation(string animation)
-        {
-            if (animUtil != null)
-            {
+        private void runAnimation(string animation) {
+            if (animUtil != null) {
                 animUtil?.StartAnimation(new AnimationMetaData() { Animation = animation, Code = animation });
             }
         }
 
-        private void playSound()
-        {
+        private void playSound() {
             Api.World.PlaySoundAt(AssetLocation.Create("sounds/effect/watering"), Pos, 0, null, false, 12, 0.75f);
         }
 
 
-        private int getRandomInterval()
-        {
+        private int getRandomInterval() {
             //Might not be the randomest seed ever but good enough
             Random r = new Random(Pos.GetHashCode());
             int configInterval = Math.Max(1, SprinklersModSystem.config.minIntervalInMillis);
             return r.Next(configInterval, configInterval + 5000);
         }
 
-        private int determineStats(bool range)
-        {
+        private int determineStats(bool range) {
             string prefix = Block.Code.Path.Substring(0, 5); //This feels a bit illegal, but hey, it's a unique identifier
-            switch (prefix)
-            {
+            switch (prefix) {
                 case "t_one": return range ? SprinklersModSystem.config.tOneSprinklerRange : SprinklersModSystem.config.tOneSprinklerVolume;
                 case "t_two": return range ? SprinklersModSystem.config.tTwoSprinklerRange : SprinklersModSystem.config.tTwoSprinklerVolume;
                 case "t_thr": return range ? SprinklersModSystem.config.tThreeSprinklerRange : SprinklersModSystem.config.tThreeSprinklerVolume;
--- SprinklersMod/Blocks/BlockSprinkler.cs
+++ SprinklersMod/Blocks/BlockSprinkler.cs
@@ -7,39 +7,33 @@ using Vintagestory.API.Util;
 using System.Collections.Generic;
 using System;
 
-namespace SprinklersMod.Blocks
-{
-    internal class BlockSprinkler : Block
-    {
+namespace SprinklersMod.Blocks {
+    internal class BlockSprinkler : Block {
 
         /*  This is different from the Water Consumption Variable of the config
         *   This one determines how much Watering Can Time is reduced each time
         *   the sprinkler gets filled */
-        const float WATER_CONSUMPTION = 2.0f;
+        const float WATERING_CAN_CONSUMPTION = 2.0f;
 
         WorldInteraction[] interactions;
 
-        public override void OnLoaded(ICoreAPI api)
-        {
+        public override void OnLoaded(ICoreAPI api) {
             base.OnLoaded(api);
 
             if (api.Side != EnumAppSide.Client) return;
 
             string dir = Code.Path.Contains("down") ? "down" : "up";
 
-            interactions = ObjectCacheUtil.GetOrCreate(api, "sprinklerInteractions" + dir, () =>
-            {
+            interactions = ObjectCacheUtil.GetOrCreate(api, "sprinklerInteractions" + dir, () => {
                 List<ItemStack> list = getApplicableContainers();
 
                 WorldInteraction[] interactions = {
-                    new WorldInteraction()
-                    {
+                    new WorldInteraction() {
                         MouseButton = EnumMouseButton.Right,
                         ActionLangCode = "blockhelp-sprinkler-fill",
                         Itemstacks = list.ToArray()
                     },
-                    new WorldInteraction()
-                    {
+                    new WorldInteraction() {
                         MouseButton = EnumMouseButton.Right,
                         HotKeyCode = "shift",
                         ActionLangCode = "blockhelp-sprinkler-fill2",
@@ -47,11 +41,9 @@ namespace SprinklersMod.Blocks
                     }
                 };
 
-                if ("down".Equals(dir))
-                {
+                if ("down".Equals(dir)) {
                     interactions = interactions.Append(
-                        new WorldInteraction()
-                        {
+                        new WorldInteraction() {
                             MouseButton = EnumMouseButton.Right,
                             ActionLangCode = "blockhelp-sprinkler-set-range",
                             RequireFreeHand = true
@@ -63,31 +55,26 @@ namespace SprinklersMod.Blocks
             });
         }
 
-        public override void OnBlockPlaced(IWorldAccessor world, BlockPos blockPos, ItemStack byItemStack = null)
-        {
+        public override void OnBlockPlaced(IWorldAccessor world, BlockPos blockPos, ItemStack byItemStack = null) {
             base.OnBlockPlaced(world, blockPos, byItemStack);
         }
 
-        public override void OnBlockBroken(IWorldAccessor world, BlockPos pos, IPlayer byPlayer, float dropQuantityMultiplier = 1)
-        {
+        public override void OnBlockBroken(IWorldAccessor world, BlockPos pos, IPlayer byPlayer, float dropQuantityMultiplier = 1) {
             base.OnBlockBroken(world, pos, byPlayer, dropQuantityMultiplier);
         }
 
-        public override bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
-        {
+        public override bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel) {
             BlockEntitySprinkler blockEntitySprinkler = world.BlockAccessor.GetBlockEntity<BlockEntitySprinkler>(blockSel.Position);
             //Check if a downwards facing sprinkler is being interacted with by hand
-            if (blockEntitySprinkler.isDownFacing && byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack == null)
-            {
+            if (blockEntitySprinkler.isDownFacing && byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack == null) {
                 blockEntitySprinkler.adjustVertRange();
                 return true;
             }
             //If not caught by the above block, resume normal behavior
-            if (blockEntitySprinkler.getMissingWater() > 0)
-            {
+            if (blockEntitySprinkler.getMissingWater() > 0) {
                 int water = checkSelectedItem(byPlayer);
-                if (water > 0 && api.Side == EnumAppSide.Server)
-                {
+
+                if (water > 0 && api.Side == EnumAppSide.Server) {
                     playFillSound(byPlayer);
                     blockEntitySprinkler.fillWater(water);
                 }
@@ -96,8 +83,7 @@ namespace SprinklersMod.Blocks
 
         }
 
-        public override WorldInteraction[] GetPlacedBlockInteractionHelp(IWorldAccessor world, BlockSelection selection, IPlayer forPlayer)
-        {
+        public override WorldInteraction[] GetPlacedBlockInteractionHelp(IWorldAccessor world, BlockSelection selection, IPlayer forPlayer) {
             return interactions.Append(base.GetPlacedBlockInteractionHelp(world, selection, forPlayer));
         }
 
@@ -106,42 +92,34 @@ namespace SprinklersMod.Blocks
         //
         // Returns the amount of water left to be used
         //
-        public int checkSelectedItem(IPlayer byPlayer)
-        {
-            if (byPlayer.InventoryManager.ActiveHotbarSlot == null || byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack == null)
-            {
+        public int checkSelectedItem(IPlayer byPlayer) {
+            if (byPlayer.InventoryManager.ActiveHotbarSlot == null || byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack == null) {
                 return 0;
             }
+            
             var itemStack = byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack;
-            if (EnumItemClass.Block == itemStack.Class)
-            {
+            if (EnumItemClass.Block == itemStack.Class) {
                 Block currentlyHeldBlock = itemStack.Block;
-                if (currentlyHeldBlock is BlockWateringCan)
-                {
-                    BlockWateringCan bw = (BlockWateringCan)currentlyHeldBlock;
+                if (currentlyHeldBlock is BlockWateringCan) {
+                    BlockWateringCan bw = (BlockWateringCan) currentlyHeldBlock;
                     float content = bw.GetRemainingWateringSeconds(itemStack);
                     bool emptyAll = byPlayer.Entity.Controls.ShiftKey;
-                    if (!emptyAll && content - WATER_CONSUMPTION > 0f)
-                    {
-                        bw.SetRemainingWateringSeconds(itemStack, bw.GetRemainingWateringSeconds(itemStack) - WATER_CONSUMPTION);
+                    if (!emptyAll && content - WATERING_CAN_CONSUMPTION > 0f) {
+                        bw.SetRemainingWateringSeconds(itemStack, bw.GetRemainingWateringSeconds(itemStack) - WATERING_CAN_CONSUMPTION);
                         return 10;
-                    } else if (emptyAll)
-                    {
+                    } else if (emptyAll) {
                         int initialStackSize = (int) bw.GetRemainingWateringSeconds(itemStack);
                         bw.SetRemainingWateringSeconds(itemStack, 0f);
                         return Math.Min(initialStackSize * 5, 150);
                     }
                 }
-                else if (currentlyHeldBlock is BlockLiquidContainerBase)
-                {
+                else if (currentlyHeldBlock is BlockLiquidContainerBase) {
                     //This should cover all containers able to hold water (Buckets, Jugs, Bowls etc.)
-                    BlockLiquidContainerBase bl = (BlockLiquidContainerBase)currentlyHeldBlock;
-                    if (bl.GetContent(itemStack) != null)
-                    {
+                    BlockLiquidContainerBase bl = (BlockLiquidContainerBase) currentlyHeldBlock;
+                    if (bl.GetContent(itemStack) != null) {
                         int mult = byPlayer.Entity.Controls.ShiftKey ? 10 : 1;
                         ItemStack contents = bl.GetContent(itemStack);
-                        if ("game:waterportion".Equals(contents.Item.Code))
-                        {
+                        if ("game:waterportion".Equals(contents.Item.Code)) {
                             float maxConsumption = 1.0f * mult;
                             int initialStackSize = contents.StackSize;
                             bl.TryTakeLiquid(itemStack, 1.0f * maxConsumption);
@@ -153,16 +131,13 @@ namespace SprinklersMod.Blocks
             return 0;
         }
 
-        private List<ItemStack> getApplicableContainers()
-        {
+        private List<ItemStack> getApplicableContainers() {
             List<ItemStack> list = new List<ItemStack>();
 
             string[] itemCodes = { "game:woodbucket", "game:jug-brown-fired", "game:bowl-red-fired", "game:wateringcan-red-fired" };
-            foreach (string code in itemCodes)
-            {
+            foreach (string code in itemCodes) {
                 Block b = api.World.GetBlock(new AssetLocation(code));
-                if (b != null)
-                {
+                if (b != null) {
                     ItemStack i = new ItemStack(b, 1);
                     list.Add(i);
                 }
@@ -171,8 +146,7 @@ namespace SprinklersMod.Blocks
             return list;
         }
         
-        private void playFillSound(IPlayer player)
-        {
+        private void playFillSound(IPlayer player) {
             api.World.PlaySoundFor(AssetLocation.Create("sounds/effect/water-fill.ogg"), player, true, 3.0f, 0.6f);
         }
     }
